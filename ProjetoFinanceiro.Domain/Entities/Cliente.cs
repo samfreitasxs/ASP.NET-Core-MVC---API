94b71fe467425e9317e7b70f34f6d8253e5fa043
samfreitasxs <samuel.sfdf@gmail.com>
2023-04-20 17:02:58 -0300

    feat: Criacao de classe da Entidade

---
+++ ProjetoFinanceiro.Domain/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinanceiro.Domain.Entities
+{
+    public class Cliente
+    {
+        private int ClienteId { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+    }
+}
